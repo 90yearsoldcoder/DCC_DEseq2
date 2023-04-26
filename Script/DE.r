f8c19102b33e017ff0111676903480c04423e693
“” <minty@bu.edu>
2023-04-26 15:10:30 -0400

    update Differential Exp script

--- Script/DE.r
+++ Script/DE.r
@@ -1,12 +1,14 @@
 library(DESeq2)
+library(tidyverse)
+library(dplyr)
 
 
 # Read in count data
-countData <- read.delim("your_file.tsv", header = TRUE, row.names = 1)
+countData <- read.delim("/restricted/projectnb/ncrna/minty/samb_data4/DCC_DEseq2/Processing/CircRNACount_prepared.tsv", header = TRUE, row.names = 1)
 
 
 # Read the colData from a TSV file
-colData <- read.delim("colData.tsv", stringsAsFactors = FALSE)  # adjust file name and options as needed
+colData <- read.delim("/restricted/projectnb/ncrna/minty/samb_data4/DCC_DEseq2/Processing/group_info.csv", stringsAsFactors = FALSE,sep=",")  # adjust file name and options as needed
 
 # Set the row names of colData to match the sample names
 rownames(colData) <- colData$sampleID
@@ -14,14 +16,100 @@ rownames(colData) <- colData$sampleID
 # Remove the sampleID column if not needed
 colData$sampleID <- NULL
 
+# Reorder colData based on colnames of countData
+ordered_colData <- colData[colnames(countData), , drop = FALSE]
 
-dds <- DESeqDataSetFromMatrix(countData, colData, design = ~ Group)
+dds <- DESeqDataSetFromMatrix(countData, ordered_colData, design = ~ group)
 # Run DESeq2 analysis
 dds <- DESeq(dds)
-# Replace 'Group1' and 'Group2' with the actual group names you want to compare
-result <- results(dds, contrast = c("Group", "Group1", "Group2"))
 
-# Filter for statistically significant differentially expressed genes
-DEGs <- subset(result, padj < 0.05 & abs(log2FoldChange) > 1)
+
+
+
+generate_DEG_table <- function(dds, contrast, p_value_flag = FALSE, res_path, sig_path) {
+  #res_path stores all gene DE result
+  #sig_path stores the genes passing the filter
+  
+  # Generate results table for specified contrast
+  result <- results(dds, contrast = contrast)
+  
+  # Filter for either p-value or adjusted p-value as specified
+  if (p_value_flag) {
+    DEGs <- subset(result, pvalue < 0.05 & abs(log2FoldChange) > 1)
+  } else {
+    DEGs <- subset(result, padj < 0.05 & abs(log2FoldChange) > 1)
+  }
+  
+  # Convert results table to a tibble, add gene names as a column, and sort by p-value
+  res_table <- as_tibble(result) %>%
+    mutate(gene = rownames(result)) %>%
+    relocate(gene) %>%
+    arrange(pvalue)
+  
+  DEGs_table <- as_tibble(DEGs) %>%
+    mutate(gene = rownames(DEGs)) %>%
+    relocate(gene) %>%
+    arrange(pvalue)
+  
+  # Save the table to output_path
+  write_csv(res_table, res_path)
+  write_csv(DEGs_table, sig_path)
+  
+  # Return the tibble
+  return(res_table)
+}
+
+plot_volcano_pvalue <- function(res_table, title, fig_path){
+  data <- mutate(
+    res_table,
+    `-log10(p_value)`=-log10(pvalue),
+    `p_value<0.05`=pvalue<0.05
+  ) 
+  plot <- ggplot(data, aes(x=log2FoldChange,y=`-log10(p_value)`,color=`p_value<0.05`)) +
+    ggtitle(title) +
+    geom_point()
+  
+  #save the picture
+  print(paste0(fig_path, "/", title, ".png"))
+  ggsave(paste0(fig_path, "/", title, ".png"), plot)
+  
+}
+
+
+# control1 vs otao1
+table <- generate_DEG_table(dds, contrast =  c("group", "Control1", "otao1"), p_value_flag = TRUE, 
+                   res_path = "/restricted/projectnb/ncrna/minty/samb_data4/DCC_DEseq2/Result/control1_vs_otao1.csv", 
+                  sig_path = "/restricted/projectnb/ncrna/minty/samb_data4/DCC_DEseq2/Result/control1_vs_otao1_sig.csv"            
+                                      )
+plot_volcano_pvalue(table, "control1_vs_otao1", "/restricted/projectnb/ncrna/minty/samb_data4/DCC_DEseq2/Result")
+
+# control2 vs otao2
+table <- generate_DEG_table(dds, contrast =  c("group", "Control2", "otao2"), p_value_flag = TRUE, 
+                   res_path = "/restricted/projectnb/ncrna/minty/samb_data4/DCC_DEseq2/Result/control2_vs_otao2.csv", 
+                   sig_path = "/restricted/projectnb/ncrna/minty/samb_data4/DCC_DEseq2/Result/control2_vs_otao2_sig.csv"            
+)
+plot_volcano_pvalue(table, "control2_vs_otao2", "/restricted/projectnb/ncrna/minty/samb_data4/DCC_DEseq2/Result")
+
+# control1 vs control2
+table <- generate_DEG_table(dds, contrast =  c("group", "Control1", "Control2"), p_value_flag = TRUE, 
+                   res_path = "/restricted/projectnb/ncrna/minty/samb_data4/DCC_DEseq2/Result/control1_vs_control2.csv", 
+                   sig_path = "/restricted/projectnb/ncrna/minty/samb_data4/DCC_DEseq2/Result/control1_vs_control2_sig.csv"            
+)
+plot_volcano_pvalue(table, "control1_vs_control2", "/restricted/projectnb/ncrna/minty/samb_data4/DCC_DEseq2/Result")
+
+# otao1 vs otao2
+table <- generate_DEG_table(dds, contrast =  c("group", "otao1", "otao2"), p_value_flag = TRUE, 
+                   res_path = "/restricted/projectnb/ncrna/minty/samb_data4/DCC_DEseq2/Result/otao1_vs_otao2.csv", 
+                   sig_path = "/restricted/projectnb/ncrna/minty/samb_data4/DCC_DEseq2/Result/otao1_vs_otao2_sig.csv"            
+)
+plot_volcano_pvalue(table, "otao1_vs_otao2", "/restricted/projectnb/ncrna/minty/samb_data4/DCC_DEseq2/Result")
+
+
+
+
+
+
+
+
 
 
