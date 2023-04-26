b0d2e899900f2e10b8c01d04e075adb567de0d74
“” <minty@bu.edu>
2023-04-26 15:12:00 -0400

    add annotation bash script

---
+++ Script/anotate.sh
@@ -0,0 +1,10 @@
+#zcat  gencode.v19.annotation.gtf.gz | grep   protein_coding |perl -alne '{next unless $F[2] eq "gene" ;/gene_name \"(.*?)\";/; print "$F[0]\t$F[3]\t$F[4]\t$1" }' >protein_coding.hg37.position
+
+perl -p -i -e 's/ /\t/g' ../Processing/CircRNA.bed
+
+#bedtools intersect -a RBP_sig7.bed  -b /restricted/projectnb/casa/mtLin/RBP_SNP/768/protein_coding.hg37.position  -wa -wb | bedtools groupby -i - -g 1-4 -c 7 -o collapse >RBP_sig7_gene.tsv
+
+module load bedtools
+
+bedtools intersect -a ../Processing/CircRNA.bed  -b /restricted/projectnb/casa/mtLin/RBP_SNP/768/protein_coding.hg37.position  -wa -wb > ../Processing/CircRNA_anotated.bed
+
